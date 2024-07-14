66cc3bb8b973eb386cb119df22f4de18bf5fe9e2
leandro <leandro_antunes28@hotmail.com>
2024-07-13 23:11:09 -0300

    removendo alguns erros e exceptions e melhorando metodos de contas

--- AGENCIA-BANCARIA/entidades/ContaCorrente.js
+++ AGENCIA-BANCARIA/entidades/ContaCorrente.js
@@ -1,18 +1,22 @@
 const Conta = require('./Conta.js');
 
 class ContaCorrente extends Conta{
-    constructor(numeroDaConta,titular, saldo,taxaDeJurosInicial, tipoDeConta){
+    constructor(numeroDaConta,titular, saldo, tipoDeConta){
         super(numeroDaConta,titular, saldo);
-        this.taxaDeJuros = taxaDeJurosInicial;
         this.tipoDeConta = tipoDeConta;
     }
-    //como juros tem bastante variacao e podem ser aplicados em diferentes momentos,
-    //foi colocado como o cliente responsavel por determinar a taxa de juros,
-    //mas isso é feito internamente dentro do banco levando em conta o perfil do cliente
+    // Como juros tem bastante variacão e podem ser aplicados em diferentes momentos,
+    // foi colocado como o cliente responsavel por determinar a taxa de juros,
+    // mas isso é feito internamente dentro do banco considerando o perfil do cliente
     // e a situação economica do mercado.
+    // Juros não podem ser negativos
     aplicaJuros(taxaDeJuros){
+        if(taxaDeJuros < 0){
+            console.log('Juros não podem ser negativos');
+            return;
+        }
         this.saldo += this.saldo * taxaDeJuros;
-        console.log(`Juros aplicados com sucesso, saldo atual: R$${this.saldo}`);
+        console.log(`Juros aplicados com sucesso na sua conta ${this.titular}, saldo atual: R$${this.saldo}`);
     }
 }
 
