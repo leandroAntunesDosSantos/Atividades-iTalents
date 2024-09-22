6fc0428240c1c7f8abb84aad5e9e9343ff71c71f
leandro <leandro_antunes28@hotmail.com>
2024-09-21 23:08:00 -0300

    atividade front-end 1 - calculadora

---
+++ Bootcamp-Front-End/CALCULADORA/Calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora {
+    somar(num1, num2) {
+        return Number(num1) + Number(num2);
+    }
+
+    subtrair(num1, num2) {
+        return Number(num1) - Number(num2);
+    }
+
+    dividir(num1, num2) {
+        return Number(num1) / Number(num2);
+    }
+
+    multiplicar(num1, num2) {
+        return Number(num1) * Number(num2);
+    }
+} 
